2e02d8d006b76a53f29d326a4e93b8920530ec23
vietpham94 <viet220994@gmail.com>
2022-05-16 15:01:50 +0700

    Finish helpdesk detail

--- src/app/services/help-desk/help-desk.service.ts
+++ src/app/services/help-desk/help-desk.service.ts
@@ -20,24 +20,37 @@ export class HelpDeskService {
   ) {
   }
 
-  get passedHelpdesk(): HelpDesk {
-    return this._passedHelpdesk;
-  }
-
-  set passedHelpdesk(value: HelpDesk) {
-    this._passedHelpdesk = value;
-  }
-
   public getListHelpDeskCategory() {
     return this.apiService.get(Constants.apiRestEndPoints.helpdeskCategory, {
       headers: this.authService.getAuthHeader
-    });
+    }, 'getListHelpDeskCategory');
   }
 
   public getListHelpDesk(params?: SearchConditions) {
     return this.apiService.get(Constants.apiRestEndPoints.helpdesk, {
       headers: this.authService.getAuthHeader,
       params: params
-    });
+    }, 'getListHelpDesk');
+  }
+
+  public getDetailHelpdesk(helpdeskId: number) {
+    return this.apiService.get(Constants.apiRestEndPoints.helpdeskDetail + helpdeskId, {
+      headers: this.authService.getNoAuthHeader()
+    }, 'getDetailHelpdesk');
+  }
+
+  public getHelpdeskHtmlContent(helpdeskId: number) {
+    return this.apiService.get(Constants.apiRestEndPoints.helpdeskHtmlContent, {
+      headers: this.authService.getNoAuthHeader(),
+      params: {id: helpdeskId}
+    }, 'getDetailHelpdesk');
+  }
+
+  get passedHelpdesk(): HelpDesk {
+    return this._passedHelpdesk;
+  }
+
+  set passedHelpdesk(value: HelpDesk) {
+    this._passedHelpdesk = value;
   }
 }
