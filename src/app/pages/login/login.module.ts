7e7fd516ffe8a4d527131bd2a4bb554f8be3853a
nvtuan <tuannvth1904004@fpt.edu.vn>
2022-05-19 14:48:41 +0700

    fix selectable

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import {HeaderFooterModule} from '../../components/header-footer/header-footer.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    HeaderFooterModule
   ],
   declarations: [LoginPage]
 })
