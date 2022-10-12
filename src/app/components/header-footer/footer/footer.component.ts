32bad0c0728f7bddf119019db13f40fb56669934
vietpham94 <viet220994@gmail.com>
2022-10-12 14:54:08 +0700

    Fix footer style activate button

--- src/app/components/header-footer/footer/footer.component.ts
+++ src/app/components/header-footer/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
+import {Constants} from '../../../common/constants';
 
 @Component({
   selector: 'app-footer',
@@ -7,13 +9,33 @@ import {Router} from '@angular/router';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  active: string;
 
   constructor(
     private router: Router,
   ) {
+    try {
+      this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+        const currentPageInfo = Constants.getPageInfoByRouterLink(event.urlAfterRedirects);
+        switch (currentPageInfo?.title) {
+          case Constants.pagesTitle.home:
+            this.active = 'home-active';
+            break;
+          case Constants.pagesTitle.search:
+            this.active = 'search-active';
+            break;
+          case Constants.pagesTitle.suggestion:
+            this.active = 'suggestion-active';
+            break;
+        }
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
   }
 
   navigateTo(routerLink: string) {
+    this.active = routerLink?.length > 0 ? routerLink + '-active' : 'home-active';
     try {
       this.router.navigateByUrl(routerLink);
     } catch (e) {
